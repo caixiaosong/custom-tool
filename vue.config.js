c17560d8db0df00af4d4f5fb40bd5458ba0f14f0
caixiaosong <895180002@qq.com>
2019-09-08 22:05:05 +0800

    引入iview，添加一个iview插件的入口login

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,8 @@
 module.exports = {
-    configureWebpack: {
-      devtool: 'source-map'
-    }
-  }
+  chainWebpack: config => {
+    //参数 prefix 设置为 true 后，所有 iView 组件标签名都可以使用前缀 i-
+    //所以 prefix: false 则是关闭前缀i-
+    //非 template/render 模式下，任然需要 i-
+    config.module.rule("iview-loader").test(/\.vue$/).use('iview-loader').loader("iview-loader").options({ prefix: false }).end()
+  },
+}

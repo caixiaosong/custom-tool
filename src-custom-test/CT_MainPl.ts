1ae4242c74b20ea204392c42182d2771b2c97482
caixiaosong <895180002@qq.com>
2019-09-17 17:49:50 +0800

    让测试输出友好点

--- src-custom-test/CT_MainPl.ts
+++ src-custom-test/CT_MainPl.ts
@@ -1,14 +1,24 @@
-
 class CT_MainPl {
     public run() {
-        console.log(`0.1 + 0.2 === 0.3 :${(0.1 + 0.2 === 0.3)}`);
-        console.log(`0.1 + 0.2 - 0.3 <Number.EPSILON :${(Math.abs(0.1 + 0.2 - 0.3) < Number.EPSILON)}`);
-        console.log((-0 === 0));
-
-        var i;
-        console.log((i === null) + "null :" + Number(null));
+        this.testNumber();
+        this.testNullOrUndefined();
     }
 
+
+    private testNumber(): void {
+        //浮点数相加不能使用===
+        console.log(`0.1 + 0.2 === 0.3 :${(0.1 + 0.2 === 0.3)}`);
+        // Number.EPSILON,最小精确度
+        console.log(`0.1 + 0.2 - 0.3 < Number.EPSILON :${(Math.abs(0.1 + 0.2 - 0.3) < Number.EPSILON)}`);
+
+        console.log("整数相加可以采用===" + (3 - 1 === 2));
+    }
+
+    private testNullOrUndefined(): void {
+        var i;//undefined
+        console.log("i === null : " + (i === null));
+        console.log("Number(null) :" + Number(null));
+    }
 }
 
 new CT_MainPl().run();

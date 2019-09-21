12baf3a8d1b2559f399f9987ead80d3ce13b5875
caixiaosong <895180002@qq.com>
2019-09-21 16:39:52 +0800

    添加自定义测试

--- src-custom-test/CT_MainPl.ts
+++ src-custom-test/CT_MainPl.ts
@@ -1,7 +1,76 @@
 class CT_MainPl {
     public run() {
-        this.testNumber();
-        this.testNullOrUndefined();
+        // this.testNumber();
+        // this.testNullOrUndefined();
+        // this.testNickFuncName();
+        // this.testFuncParm();
+        // this.testArrLike();
+        this.testProxy();
+
+    }
+
+
+    private testProxy() {
+        //被代理类
+        class Count {
+            public count: number = 0;
+            public proxy: any = undefined;
+        }
+
+        //代理处理器，只代理了get,set
+        let proxyHandler: ProxyHandler<Count> = {
+            get: function (target, key, receiver) {
+                console.log('testPorxy get');
+                return Reflect.get(target, key, receiver);
+            },
+            set: function (target, key, value, receiver) {
+                console.log('testPorxy set');
+                return Reflect.set(target, key, value, receiver);
+            }
+        };
+
+        //代理实例
+        var countProxy: Count = new Proxy(new Count(), proxyHandler);
+
+        console.log(++countProxy.count);
+        console.log(countProxy.count);
+        console.log(`count in countProxy: ${'count' in countProxy}`);
+
+    }
+
+    private testArrLike() {
+        let arrayLike = {
+            '0': 'a',
+            '1': 'b',
+            '2': 'c',
+            length: 3 // 必须要有length属性
+        };
+
+
+        // ES6的写法
+        let arr2 = Array.from(arrayLike); // ['a', 'b', 'c']
+
+        console.log("arr2 length:" + arr2);
+    }
+
+    private testFuncParm(): void {
+        let fun = this.testFunc;
+        fun.call([2, 3, 4]);//严格模式下传递不过去
+    }
+    private testFunc() {
+        console.log(arguments);
+    }
+
+    private testNickFuncName(): void {
+        let newFunc = function () {
+
+        }
+        console.log("newFunc:" + newFunc.name);
+
+        let tmpFunc = newFunc;//重新赋值不会改变名字，
+
+        console.log("newFunc:" + newFunc.name);
+        console.log("tmpFunc:" + tmpFunc.name);
     }
 
 

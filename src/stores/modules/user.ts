5dd17fec21c30ac92c74b0246e2798f8c047d8d8
caixiaosong <895180002@qq.com>
2019-09-28 15:21:35 +0800

    添加store模版

---
+++ src/stores/modules/user.ts
@@ -0,0 +1,23 @@
+
+export default {
+    // 导入子模块
+    modules: {
+
+    },
+    // 成员变量设置
+    state: {
+
+    },
+    // 外部可以获取得到的配置
+    getters: {
+
+    },
+    // 同步方法配置
+    mutations: {
+
+    },
+    // 异步方法配置
+    actions: {
+
+    }
+}
--- src/stores/store.ts
+++ src/stores/store.ts
@@ -1,18 +1,26 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import user from './modules/user';
+
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
+  // 导入子模块
   modules: {
+    user
   },
+  // 成员变量设置
   state: {
 
   },
+  // 同步方法配置
   mutations: {
 
   },
+  // 异步方法配置
   actions: {
 
   }
 });
+

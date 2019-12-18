2164e060954393f717cb1553344e182d8edec8d0
caixiaosong <895180002@qq.com>
2019-12-18 14:06:33 +0800

    升级ivew到view 4.0

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,10 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './stores/store';
-import iview from 'iview';
-import 'iview/dist/styles/iview.css';
+import ViewUI from 'view-design';
+import 'view-design/dist/styles/iview.css';
 
-Vue.use(iview); // 使用所有iview组件
+Vue.use(ViewUI); // 使用所有iview组件
 Vue.config.productionTip = false;
 
 new Vue({
